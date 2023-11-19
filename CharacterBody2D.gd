be6c775c498ac06183d69765e0c85de78e11367f
kKapalka <k.kapalka@hotmail.com>
2023-11-19 13:55:07 +0100

    fix collision and triggers

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -89,11 +89,12 @@ func addToInventory(itemName, amount):
 	print(inventory)
 
 
-func _on_area_2d_body_entered(body):
-	if (body.has_method("onTriggerEnter")):
-		body.onTriggerEnter(self)
+func _on_area_2d_area_entered(area):
+	print(area)
+	if (area.get_parent().has_method("onTriggerEnter")):
+		area.get_parent().onTriggerEnter(self)
 
 
-func _on_area_2d_body_exited(body):
-	if (body.has_method("onTriggerExit")):
-		body.onTriggerExit(self)
+func _on_area_2d_area_exited(area):
+	if (area.get_parent().has_method("onTriggerExit")):
+		area.get_parent().onTriggerExit(self)
