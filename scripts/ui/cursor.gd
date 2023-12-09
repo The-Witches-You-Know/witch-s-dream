11e56d146a0deddae659ddcf0c47ad88b6b7d091
Resben <bendavidhawkins@gmail.com>
2023-12-09 14:20:52 +0800

    [Fix] Quick cursor fix

--- scripts/ui/cursor.gd
+++ scripts/ui/cursor.gd
@@ -7,5 +7,5 @@ func _process(delta):
 	self.global_position = get_viewport().get_mouse_position()
 	
 func _input(event):
-	if event is InputEventMouseButton and event.is_pressed():
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
 		$AnimationPlayer.play("click")
